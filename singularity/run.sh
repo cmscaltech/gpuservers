099c4b3aa5e848828962ea909b5b4a2149357a84
Jean-Roch Vlimant <vlimant@cern.ch>
2019-09-18 12:35:10 -0700

    move to ceph

--- singularity/run.sh
+++ singularity/run.sh
@@ -28,7 +28,7 @@ if [ -d "/storage" ] ; then
 fi
 
 ex=""
-img=/bigdata/shared/Software/singularity/ibanks/edge.simg
+img=/storage/group/gpu/software/singularity/ibanks/edge.simg
 if [ ! -z $1 ] ; then
     if [ -r $1 ] ; then
 	echo using $1 as image
