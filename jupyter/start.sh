d6b8ace82be4761923d019d6127946cd7a1a57bd
Jean-Roch Vlimant <vlimant@cern.ch>
2019-09-19 07:56:34 -0700

    verify existence of notebook

--- jupyter/start.sh
+++ jupyter/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
-jupyter notebook --config   /storage/group/gpu/software/gpuservers/jupyter/jupyter_notebook_config_S.py
+#check for any already existing server
+existing=`ps -f -u $USER | grep jupyter | grep  notebook | grep config | grep -v grep | wc -l `
+echo $existing
+if [ $existing -eq "0" ] ; then
+    jupyter notebook --config   /storage/group/gpu/software/gpuservers/jupyter/jupyter_notebook_config_S.py
+else
+    echo "user $USER has existing notebooks running already"
+    ps -f -u $USER | grep jupyter
+    ## should this be killed by default?
+fi
