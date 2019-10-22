8fa22d6bec5e2d4dfe5b7157dd099bf7960b6ac9
Jean-Roch Vlimant <vlimant@cern.ch>
2019-10-22 01:36:39 -0700

    change home directory

--- jupyter/jupyter_notebook_config_S.py
+++ jupyter/jupyter_notebook_config_S.py
@@ -263,7 +263,7 @@ c.NotebookApp.keyfile = '/etc/grid-security/letsencrypt/privkey.pem'
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = u'/nfshome/'
+c.NotebookApp.notebook_dir = u'/storage/user/{}'.format(os.environ.get('USER',''))
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
