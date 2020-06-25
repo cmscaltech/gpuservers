b0136f65421e15edaaa3e6e58db87d5146c5c096
Jean-Roch Vlimant <vlimant@cern.ch>
2020-06-25 13:14:47 -0700

    using uid as port

--- jupyter/jupyter_notebook_config_S.py
+++ jupyter/jupyter_notebook_config_S.py
@@ -293,7 +293,8 @@ c.NotebookApp.notebook_dir = '/storage/user/{}'.format(os.environ.get('USER','')
 
 ## The port the notebook server will listen on.
 
-c.NotebookApp.port = json.loads(open('/storage/group/gpu/software/gpuservers/jupyter/ports').read())[os.getenv('USER')]
+#c.NotebookApp.port = json.loads(open('/storage/group/gpu/software/gpuservers/jupyter/ports').read())[os.getenv('USER')]
+c.NotebookApp.port = os.getuid()
 print(os.getenv('USER'),"on port",c.NotebookApp.port)
 ## The number of additional ports to try if the specified port is not available.
 #c.NotebookApp.port_retries = 50
